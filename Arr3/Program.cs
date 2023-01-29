827facf6580e75cea5f9f17da082051227fb598b
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 20:24:10 +0300

    add Набрали...

---
+++ Arr3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Lenght;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+            Console.WriteLine(col[position]);
+            position++;
+    }
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
