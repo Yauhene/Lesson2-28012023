6aa34a40271ccc4487e1faa0d5d1368d118da906
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 19:17:33 +0300

    add Упростили запись

--- Funk02/Program.cs
+++ Funk02/Program.cs
@@ -18,10 +18,14 @@ int a3 = 13;
 int b3 = 2311;
 int c3 = 313;
 
-int max1 = Max(a1,b1,c1);
-int max2 = Max(a2,b2,c2);
-int max3 = Max(a3,b3,c3);
+//int max1 = Max(a1,b1,c1);
+//int max2 = Max(a2,b2,c2);
+//int max3 = Max(a3,b3,c3);
 
-int max = Max(max1,max2,max3);
+// можно было бы результ. Max написать в одн строчку, но так нагляднее
+int max = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
 
 Console.WriteLine(max);
