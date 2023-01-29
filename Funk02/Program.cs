8833c671996c226713342436a45188cc83b22a15
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 19:13:45 +0300

    add Выщемил косяки

--- Funk02/Program.cs
+++ Funk02/Program.cs
@@ -15,12 +15,12 @@ int a2 = 12;
 int b2 = 23;
 int c2 = 33;
 int a3 = 13;
-int b3 = 23;
-int c3 = 33;
+int b3 = 2311;
+int c3 = 313;
 
 int max1 = Max(a1,b1,c1);
 int max2 = Max(a2,b2,c2);
-int max2 = Max(a3,b3,c3);
+int max3 = Max(a3,b3,c3);
 
 int max = Max(max1,max2,max3);
 
