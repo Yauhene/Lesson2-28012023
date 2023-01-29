f1212459162f9913590e1dae305893351596b10b
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-29 20:10:54 +0300

    add Теперь хорошо

--- Arr02/Program.cs
+++ Arr02/Program.cs
@@ -1,14 +1,17 @@
 ﻿// Поиск индекса элемента с заданным значением
 
-int[] array = {1,12,31,4,15,16,17,18};
+int[] array = {1,12,18,4,15,16,17,18};
 int n = array.Length;
-int find = 4;
+int find = 18;
 
 int index = 0;
 
 while (index < n)
 {
     if(array[index] == find)
-    Console.WriteLine("index = " + index);
+    {
+        Console.WriteLine("index = " + index);
+        break;
+    }
     index++; // index=index+1
 }
